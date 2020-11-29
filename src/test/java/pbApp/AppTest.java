46e8c67b3f058884edb9d4e898c24596d8853a9e
sceran <suleymanceran33@gmail.com>
2020-11-29 21:57:31 +0300

    Getting ready for deployment to heroku

--- src/test/java/pbApp/AppTest.java
+++ src/test/java/pbApp/AppTest.java
@@ -17,27 +17,5 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
-    @Test
-   public void testFound() {
-      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertTrue(App.search(array, 4));
-    }
-
-    @Test
-    public void testNotFound() {
-      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertFalse(App.search(array, 5));
-    }
-
-    @Test
-    public void testEmptyArray() {
-      ArrayList<Integer> array = new ArrayList<>();
-      assertFalse(App.search(array, 1));
-    }
-
-    @Test
-    public void testNull() {
-      assertFalse(App.search(null, 1));
-    }
-
+    
 }
